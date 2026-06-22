f07795e965ba6c3b804b7a91d53fbaf7b4d4eb7f
serenity <263214231+evil-serenity@users.noreply.github.com>
2026-06-22 20:54:25 +0300

    Replace frontier with TE changelog in actions_changelogs_since_last_run.

--- Tools/actions_changelogs_since_last_run.py
+++ Tools/actions_changelogs_since_last_run.py
@@ -22,7 +22,9 @@ GITHUB_API_URL = os.environ.get("GITHUB_API_URL", "https://api.github.com")
 DISCORD_SPLIT_LIMIT = 2000
 DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL")
 
-CHANGELOG_FILE = "Resources/Changelog/Frontier.yml" # Frontier: Changelog.yml<Frontier.yml
+CHANGELOG_FILE = (
+    "Resources/Changelog/TEChangelog.yml"  # TE: Frontier.yml<TEChangelog.yml
+)
 
 TYPES_TO_EMOJI = {"Fix": "🐛", "Add": "🆕", "Remove": "❌", "Tweak": "⚒️"}
 
